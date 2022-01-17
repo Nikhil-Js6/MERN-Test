72e549574915ace75c88aea0644121794944f740
Nikhil <56617924+Nikhil-Js6@users.noreply.github.com>
2022-01-18 01:32:37 +0530

    Update verifyToken.js

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -1,10 +1,12 @@
 const jwt = require("jsonwebtoken");
 
+const HASHING_SECRET = '9e42e429fe34d87a78eab640e6ffd83445f50300738f0';
+
 function verify(req, res, next){
     const authHeader = req.headers.token;
     if(authHeader){
         const token = authHeader.split(" ")[1];
-        jwt.verify(token, process.env.HASHING_SECRET,
+        jwt.verify(token, HASHING_SECRET,
             (err, user) => {
                 if(err) {
                     res.status(403).json("Token is Invalid!");
